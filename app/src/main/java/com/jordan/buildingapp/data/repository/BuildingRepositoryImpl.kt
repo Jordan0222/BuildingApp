7b7a8f7f93be8b5b2f04122f0ecdcc2517af8eb2
asxz0 <asxz0121@gmail.com>
2022-07-03 14:33:39 +0800

    IO Dispatcher

--- app/src/main/java/com/jordan/buildingapp/data/repository/BuildingRepositoryImpl.kt
+++ app/src/main/java/com/jordan/buildingapp/data/repository/BuildingRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.jordan.buildingapp.data.repository
 
+import com.jordan.buildingapp.data.remote.Building
 import com.jordan.buildingapp.data.remote.BuildingApi
 import com.jordan.buildingapp.data.remote.BuildingItem
 import com.jordan.buildingapp.domain.repository.BuildingRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class BuildingRepositoryImpl(
@@ -10,7 +13,10 @@ class BuildingRepositoryImpl(
 ): BuildingRepository {
     override suspend fun getBuildItems(page: Int, pageSize: Int): Result<List<BuildingItem>> {
         return try {
-            val response = buildingApi.getBuildingItems()
+            var response: Building
+            withContext(Dispatchers.IO) {
+                response = buildingApi.getBuildingItems()
+            }
             val buildingItems = response.DataList
             val startingIndex = page * pageSize
             if (startingIndex + pageSize <= buildingItems.size) {
